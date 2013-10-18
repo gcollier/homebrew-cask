5d6870b458127d3ea7ba0327055814a9a87062bd
Gary Collier <gcollier@SNAL4166.local>
2013-10-18 13:54:30 -0700

    Zoc 6.5.1
    Install Zoc 6.5.1, SSH/Telnet Client and Terminal Emulator for Mac OS X

---
+++ Casks/zoc.rb
@@ -0,0 +1,7 @@
+class Zoc < Cask
+  url 'http://www.emtec.com/downloads/zoc/zoc651.dmg'
+  homepage 'http://www.emtec.com/zoc/'
+  version '6.5.1'
+  sha1 '2883e4ffe0cde1cda1bc29ba1673d07a13b6a369'
+  link 'zoc6'
+end
